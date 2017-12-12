cb06c1d5ab344fa3867a87638effe203d52fcb96
xWardo <34096821+xWardo@users.noreply.github.com>
2017-12-12 10:04:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from telegram.ext import Updater #Importas las librerias
 updater = Updater(token='458849790:AAG9dLDx5f_jNlA8NjnJl_-gTvmW2nN8nh4') #Creas el updater para mantener el bot siempre activo
 from telegram import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove, ParseMode #librerias para cambiar el teclado
 dispatcher = updater.dispatcher #Para que el updater acceda mas rapido al dispatcher
-from telegram.ext import CommandHandler #Importas ms librerias
-from telegram.ext import MessageHandler, Filters
+
 #Esto crea un registro en la consola para saber si falla algo pues que diga que es lo que falla
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -39,19 +38,21 @@ def build_menu(buttons, n_cols=1, header_buttons=None, footer_buttons=None):
 
     return menu
 
+
+
+from telegram.ext import CommandHandler #Importas más librerias
 start_handler = CommandHandler('start', start) #Haces que con el comando /start se inicie la funcion start
-dispatcher.add_handler(start_handler) #Lo aades al dispatcher
+dispatcher.add_handler(start_handler) #Lo añades al dispatcher
 #Para que repita los mensajes
 def echo(bot, update):
         bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 
-
+from telegram.ext import MessageHandler, Filters
 echo_handler = MessageHandler(Filters.text, echo)
 dispatcher.add_handler(echo_handler)
 
 def ayuda(bot, update):
-        def ayuda(bot, update):
-                bot.send_message(chat_id=update.message.chat_id, text="Comandos del bot:\n\n/nuria para ver el numero divino\n\n/AETM para ver el estado de la serie AETM\n\n/capitulito anuncia del capitulito actual, haciendo un spam de la HOSTIA PUTA JODERRR.\n /pole para polear con mucha más felicidad y facilidad \n /github pagina de github (algo momentaneo para el admin) \n /donaciones Ayuda a este bot!!")
+        bot.send_message(chat_id=update.message.chat_id, text="Comandos del bot:\n /nuria para ver el numero divino \n /AETM para ver el estado de la serie AETM \n")
 
 help_handler = CommandHandler('help', ayuda)
 ayuda_handler = CommandHandler('ayuda', ayuda)
@@ -60,7 +61,7 @@ dispatcher.add_handler(help_handler)
 
 
 
-#Si ponen un comando que no est registrado
+#Si ponen un comando que no está registrado
 def unknown(bot, update):
         bot.send_message(chat_id=update.message.chat_id, text="Lo siento, no entiendo ese comando! asegurate de escribirlo bien. /help para ver los comandos disponibles.")
 
